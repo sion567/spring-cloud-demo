f9b21abab75d49d23668d296075f13543bf1f274
sion567 <461959180@qq.com>
2016-11-03 17:28:45 +0800

    增加了异常和超时

--- say-microservice/src/main/java/cc/sion/web/SayController.java
+++ say-microservice/src/main/java/cc/sion/web/SayController.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class SayController {
     @Autowired
@@ -21,10 +23,26 @@ public class SayController {
         return "hello:"+name;
     }
 
-    @RequestMapping(value = "/sayBye" ,method = RequestMethod.GET)
-    public String sayBye() {
+    @RequestMapping(value = "/sayBye/{name}" ,method = RequestMethod.GET)
+    public String sayBye(@PathVariable String name) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        System.out.println("/say hello");
+        System.out.println("    host:"+instance.getHost());
+        System.out.println("    service_id:" + instance.getServiceId());
+
         throw new RuntimeException("sya bye bye~~");
     }
 
+    @RequestMapping(value = "/sayWaiting/{name}" ,method = RequestMethod.GET)
+    public String sayWaiting(@PathVariable String name) {
+        try {
+//            Thread.sleep(2*60*1000);
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "I hate waiting in line.";
+    }
 
 }
